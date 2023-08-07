70c38a6b829e09b3470d6f502da3837526311207
Alexey Sokolov <alexvyber@gmail.com>
2023-08-07 19:28:11 +0300

    fastify-testing-ci

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: 'lf',
+  printWidth: 200,
+  semi: false,
+  singleQuote: false,
+  arrowParens: 'always',
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSameLine: false
+}
---
+++ routes/data-store.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const registered = []
+
+module.exports = {
+  async store (user) {
+    registered.push(user)
+  },
+
+  data () {
+    return registered
+  }
+}
